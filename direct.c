3777bf99c827842b1df563a3d45875925da5aeee
YDW <osydoo@naver.com>
2017-11-17 20:27:54 +0900

    serch, end 어느정도 완료 - search에서 *옵션과 ?옵션은 구현중

--- direct.c
+++ direct.c
@@ -30,7 +30,7 @@ struct actor{//포인터 포현이 안되서 그냥 배열로 함 수정 필요,
 	int serial_number;//배우 시리얼 번호
 	char actor[20];//배우 이름
 	char sex[3];//성
-	char birth[10];//생일//왜 int형은 경고가 뜨는데 char형은 경고아 안뜨는가?
+	char birth[10];//생일-왜 int형은 경고가 뜨는데 char형은 경고아 안뜨는가?
 	char title[3][20];//대표작 제목
 	struct actor *next_actor;//다음 항목으로
 };
@@ -56,10 +56,95 @@ linklist_a *get_actor(){//동적 할당-배우
 	tmp -> next_actor = EMPTY;
 	return tmp;
 }
+// 배열 표현을 포인터로 먼저 고치고 search 만들자!
+
+
+
+void search(linklist_d **first_d, linklist_m **first_m, linklist_a **first_a){
+	//linklist_d *first_d;
+	linklist_d **tmp_d;
+	tmp_d = first_d;
+	//linklist_m *first_m;
+	linklist_m **tmp_m;
+	tmp_m = first_m;
+	//linklist_a *first_a;
+	linklist_a **tmp_a;//전부다 구조체 불러옴
+	tmp_a = first_a;
+	char option[3];//확인을 위한 임시적인 변수
+	char string[3];
+
+	printf("옵션을 입력하세요 : ");
+	scanf("%s", option);
+	printf("찾을 항목을 입력하새요 : ");
+	scanf("%s", string);
+
+	for(int i=0 ; i<3 ; i++){//option이 dma3개다 들어갈 수 있으므로 3
+		if(option[i]=='d'){
+			while(1){//모든 항목을 뒤지기 위해서 사용
+				if (*first_d == NULL) {//감독 내에서 비교
+					printf("director에 저장 된 값이 없습니다");
+					break;
+				}
+				if(strcmp(string, (*first_d)->director) == 0)//감독 이름에잇는가?
+					printf("%d:%s:%s:%s:%s, %s, %s\n", (*first_d)->serial_number, (*first_d)->director, (*first_d)->sex, (*first_d)->birth, (*first_d)->title[0], (*first_d)->title[1], (*first_d)->title[2]);
+				else if(strcmp(string, (*first_d)->sex) == 0)//성에 잇는가?
+					printf("%d:%s:%s:%s:%s, %s, %s\n", (*first_d)->serial_number, (*first_d)->director, (*first_d)->sex, (*first_d)->birth, (*first_d)->title[0], (*first_d)->title[1], (*first_d)->title[2]);
+				else if(strcmp(string, (*first_d)->birth) == 0)//생일에 있는가?
+					printf("%d:%s:%s:%s:%s, %s, %s\n", (*first_d)->serial_number, (*first_d)->director, (*first_d)->sex, (*first_d)->birth, (*first_d)->title[0], (*first_d)->title[1], (*first_d)->title[2]);
+				for(int i = 0; i < 3;i++){//대표작들에 있는가?
+					if(strcmp(string, (*first_d)->title[i]) == 0)
+						printf("%d:%s:%s:%s:%s, %s, %s\n", (*first_d)->serial_number, (*first_d)->director, (*first_d)->sex, (*first_d)->birth, (*first_d)->title[0], (*first_d)->title[1], (*first_d)->title[2]);
+				}
+				*first_d = (*first_d)->next_director;//다음 주소 값을 뒤져보자!
+			}
+			*first_d = *tmp_d;//첫번째 주소값을 보존하기 위한 코드
+		}
+		else if(option[i] == 'm'){//영화 내에서 비교
+			while(1){//모든 항목을 뒤지기 위해서 사용
+				if (*first_m == 0) {
+					printf("movie에 저장 된 값이 없습니다");
+					break;
+				}
+				if(strcmp(string, (*first_m)->title) == 0)//영화 제목에 있는가?
+					printf("%d:%s:%s:%s:%s, %s, %s\n", (*first_m)->serial_number, (*first_m)->title, (*first_m)->genre, (*first_m)->director, (*first_m)->actor[0], (*first_m)->actor[1], (*first_m)->actor[2]);
+				else if(strcmp(string, (*first_m)->genre) == 0)//장르에 있는가?
+					printf("%d:%s:%s:%s:%s, %s, %s\n", (*first_m)->serial_number, (*first_m)->title, (*first_m)->genre, (*first_m)->director, (*first_m)->actor[0], (*first_m)->actor[1], (*first_m)->actor[2]);
+				else if(strcmp(string, (*first_m)->director) == 0)//감독이름에 있는가?
+					printf("%d:%s:%s:%s:%s, %s, %s\n", (*first_m)->serial_number, (*first_m)->title, (*first_m)->genre, (*first_m)->director, (*first_m)->actor[0], (*first_m)->actor[1], (*first_m)->actor[2]);
+				for(int i = 0; i < 3;i++){//배우 이름에 있는가?
+					if(strcmp(string, (*first_m)->actor[i]) == 0)
+						printf("%d:%s:%s:%s:%s, %s, %s\n", (*first_m)->serial_number, (*first_m)->title, (*first_m)->genre, (*first_m)->director, (*first_m)->actor[0], (*first_m)->actor[1], (*first_m)->actor[2]);
+				}
+				*first_m = (*first_m)->next_movie;//다음값으로 넘어가자
+			}
+			*first_m = *tmp_m;//첫번째 주소값을 보존하기 위한 코드
+		}
+		else if(option[i] == 'a'){
+			while(1){//모든 항목을 뒤지기 위해서 사용
+				if (*first_a == 0) {
+					printf("actor에 저장 된 값이 없습니다");
+					break;
+				}
+				if(strcmp(string, (*first_a)->actor) == 0)//영화 제목에 있는가?
+					printf("%d:%s:%s:%s:%s, %s, %s\n", (*first_a)->serial_number, (*first_a)->actor, (*first_a)->sex, (*first_a)->birth, (*first_a)->title[0], (*first_a)->title[1], (*first_a)->title[2]);
+				else if(strcmp(string, (*first_a)->sex) == 0)//성에 있는가?
+					printf("%d:%s:%s:%s:%s, %s, %s\n", (*first_a)->serial_number, (*first_a)->actor, (*first_a)->sex, (*first_a)->birth, (*first_a)->title[0], (*first_a)->title[1], (*first_a)->title[2]);
+				else if(strcmp(string, (*first_a)->birth) == 0)//생일에 있는가?
+					printf("%d:%s:%s:%s:%s, %s, %s\n", (*first_a)->serial_number, (*first_a)->actor, (*first_a)->sex, (*first_a)->birth, (*first_a)->title[0], (*first_a)->title[1], (*first_a)->title[2]);
+				for(int i = 0; i < 3;i++){
+					if(strcmp(string, (*first_a)->title[i]) == 0)//대표작이름에 있는가?
+						printf("%d:%s:%s:%s:%s, %s, %s\n", (*first_a)->serial_number, (*first_a)->actor, (*first_a)->sex, (*first_a)->birth, (*first_a)->title[0], (*first_a)->title[1], (*first_a)->title[2]);
+				}
+				*first_a = (*first_a)->next_actor;//다음값으로 넘어가자
+			}
+			*first_a = *tmp_a;//초기값을 보존하자
+		}
+	}
+}
 
 int add_director(linklist_d **first_d){//감독 입력하기
-	linklist_d *tmp;
-	tmp = *first_d;
+	/*linklist_d *tmp;
+	tmp = *first_d;*/// 다시보니 왜 들어가 있는지 모르겠어서 빼봄 -> 정상실행 -> 모든add항목에서 뺌 혹시몰라서 주석 처리
 	char director[20];//감독 이름
 	char sex[3];//성
 	char birth[10];//생일
@@ -114,8 +199,8 @@ if (fp == NULL){
 }
 
 int add_movie(linklist_m **first_m){//영화 입력하기
-	linklist_m *tmp;
-	tmp = *first_m;
+	/*linklist_m *tmp;
+	tmp = *first_m;*/
 	char title[20];//영화 이름
 	char genre[10];//영화 장르
 	char director[20];//감독이름
@@ -172,8 +257,8 @@ if (fp == NULL){
 }
 
 int add_actor(linklist_a **first_a){//배우 입력하기
-	linklist_a *tmp;
-	tmp = *first_a;
+	/*linklist_a *tmp;
+	tmp = *first_a;*/
 	char actor[20];//감독 이름
 	char sex[3];//성
 	char birth[10];//생일
@@ -230,13 +315,39 @@ if (fp == NULL){
 
 int main(){
 	int i = 0;
-	printf("1");
 	linklist_d *first_d = EMPTY;
 	linklist_m *first_m = EMPTY;
 	linklist_a *first_a = EMPTY;
+	//char *option;
+	//char *string;
+	//*option = "m";
+	//*string = "1";
 
-	add_director(&first_d);
-	add_movie(&first_m);
-	add_actor(&first_a);
+	while(1){
+		system("clear");
+		printf("1.찾기 2.감독입력 3.영화입력 4.배우입력 5.종료 - ");
+		scanf("%d", &i);
+		system("clear");
+		switch(i){
+			case 1:
+				search(&first_d, &first_m, &first_a);
+				break;
+			case 2:
+				add_director(&first_d);
+				break;
+			case 3:
+				add_movie(&first_m);
+				break;
+			case 4:
+				add_actor(&first_a);
+				break;
+			case 5:
+				printf("종료합니다");
+				break;
+		}
+		if(i == 5){
+				break;
+		}
+	}
 	return 0;
 }
